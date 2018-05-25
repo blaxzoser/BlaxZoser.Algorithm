74990cc0e1aaae316d753ea1ac1bfec6f00ab5d2
Luis Alberto Pintado Goicochea <luiscol50@hotmail.com>
2018-05-25 00:30:53 -0500

    Add new class

---
+++ BlaxZoser.Algorithm/Palindromo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlaxZoser.Algorithm
+{
+    public class Palindromo
+    {
+        public bool Is(string sentence)
+        {
+            int length = sentence.Length;
+            char[] array = sentence.ToCharArray();
+            bool isPalindromo = true;
+            for (int i = 0; i <= length - 1; i++)
+            {
+                for (int j = length - 1 - i; j >= 0; j--)
+                {
+                    if (array[i] != array[j])
+                    {
+                        isPalindromo = false;
+                        break;
+                    }
+                    break;
+                }
+            }
+            return isPalindromo;
+        }
+
+    }
+}
