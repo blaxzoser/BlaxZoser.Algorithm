d79e4b40e6a8d32c5402212277434838d1f6d078
blaxzoser <luiscol50@hotmail.com>
2018-05-28 16:55:58 -0500

    Added BubbleSort

---
+++ BlaxZoser.Algorithm/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlaxZoser.Algorithm
+{  
+    public class Order
+    {
+        public int[] ApplyOrderAscWithBubbleSort(int[] array)
+        {
+            for (int i = 0; i < array.Length -1; i++)
+            {
+                for (int j = 0 ; j< array.Length -i - 1;j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+
+                }               
+            }
+            return array;
+        }
+    }
+}
---
+++ BlaxZoser.UnitTest/OrderTest.cs
@@ -0,0 +1,44 @@
+﻿using BlaxZoser.Algorithm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BlaxZoser.UnitTest
+{
+    public class OrderTest
+    {
+        [Fact]
+        public void ShouldBeOrderWithBubbleSort()
+        {
+            // arrange
+            var testArray = new int[] { 5, 1, 4, 2, 8 };
+            var fakeResult = new int[] { 1, 2, 4, 5, 8 };
+            var order = new Order();
+
+            // Act
+            var resultArray = order.ApplyOrderAscWithBubbleSort(testArray);
+
+            // Assert
+            Assert.Equal(resultArray,fakeResult);
+
+        }
+
+
+        [Fact]
+        public void ShouldBeOrderWithBubbleSortWithTenElements()
+        {
+            // arrange
+            var testArray = new int[] { 10,9,8,7,6,5,4,3,2,1};
+            var fakeResult = new int[] { 1,2,3,4,5,6,7,8,9,10};
+            var order = new Order();
+
+            // Act
+            var resultArray = order.ApplyOrderAscWithBubbleSort(testArray);
+
+            // Assert
+            Assert.Equal(resultArray, fakeResult);
+
+        }
+    }
+}
