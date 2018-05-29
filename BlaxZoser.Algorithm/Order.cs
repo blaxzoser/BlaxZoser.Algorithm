29c1f7eee309620d56e1ebab413a51077f5a7b15
blaxzoser <luiscol50@hotmail.com>
2018-05-29 00:03:12 -0500

    Added recursive in bubble sort

--- BlaxZoser.Algorithm/Order.cs
+++ BlaxZoser.Algorithm/Order.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace BlaxZoser.Algorithm
-{  
+{
     public class Order
     {
         public int[] ApplyOrderAscWithBubbleSort(int[] array)
         {
-            for (int i = 0; i < array.Length -1; i++)
+            for (int i = 0; i < array.Length - 1; i++)
             {
-                for (int j = 0 ; j< array.Length -i - 1;j++)
+                for (int j = 0; j < array.Length - i - 1; j++)
                 {
                     if (array[j] > array[j + 1])
                     {
@@ -19,9 +19,29 @@ namespace BlaxZoser.Algorithm
                         array[j + 1] = temp;
                     }
 
-                }               
+                }
             }
             return array;
         }
+
+        public void ApplyRecursiveBubbleSort(int[] array,int n)
+        {
+            if (n == 1)
+                return;
+            for (int j = 0; j < array.Length  - 1; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+
+                ApplyRecursiveBubbleSort(array, n - 1);
+
+            }
+
+        }
+
     }
 }
--- BlaxZoser.UnitTest/OrderTest.cs
+++ BlaxZoser.UnitTest/OrderTest.cs
@@ -26,11 +26,44 @@ namespace BlaxZoser.UnitTest
 
 
         [Fact]
-        public void ShouldBeOrderWithBubbleSortWithTenElements()
+        public void ShouldBeOrderWithRecursiveBubbleSortWithTenElements()
         {
             // arrange
             var testArray = new int[] { 10,9,8,7,6,5,4,3,2,1};
             var fakeResult = new int[] { 1,2,3,4,5,6,7,8,9,10};
+            var order = new Order();
+
+            // Act
+            order.ApplyRecursiveBubbleSort(testArray, testArray.Length);
+
+            // Assert
+            Assert.Equal(testArray, fakeResult);
+
+        }
+
+        [Fact]
+        public void ShouldBeOrderWithRecursiveBubbleSort()
+        {
+            // arrange
+            var testArray = new int[] { 5, 1, 4, 2, 8 };
+            var fakeResult = new int[] { 1, 2, 4, 5, 8 };
+            var order = new Order();
+
+            // Act
+             order.ApplyRecursiveBubbleSort(testArray, testArray.Length);
+
+            // Assert
+            Assert.Equal(testArray, fakeResult);
+
+        }
+
+
+        [Fact]
+        public void ShouldBeOrderWithBubbleSortWithTenElements()
+        {
+            // arrange
+            var testArray = new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            var fakeResult = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             var order = new Order();
 
             // Act
@@ -40,5 +73,6 @@ namespace BlaxZoser.UnitTest
             Assert.Equal(resultArray, fakeResult);
 
         }
+
     }
 }
